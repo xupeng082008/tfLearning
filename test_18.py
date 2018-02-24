55633c880e387f34dd29221834613fd19b7f60e9
xupeng <xupeng@btows.com>
2018-02-24 18:02:00 +0800

    add 18

--- test_18.py
+++ test_18.py
@@ -1,6 +1,9 @@
 # /usr/bin/env python3
 # -*-coding: utf-8 -*
 
+import os
+import test_13
+
 def get_input(info):
     return str(input(info))
 
@@ -23,11 +26,30 @@ def wds2json(wds):
         print('"' + str(i).replace(' ','') +'",')
 
 
+def mergesim(path):
+    if not os.path.exists(path):
+         print("invalid path")
+    out_put = 'sim_all.txt'
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isfile(file_path):
+            with open(file_path, encoding='utf-8') as sf:
+                lines = sf.readlines()
+                for line in lines:
+                    wf = open(out_put, 'a')
+                    wf.write(line + '\n')
+                    wf.close()
+    pass
+
 def main():
     info = "please input your wds:"
-    while 1:
+    """while 1:
         wds = get_input(info)
-        wds2json(wds)
+        wds2json(wds)"""
+    sim_path = '/Users/xupeng/cfg_repos/work/gjznhs/教学/新框架编程说明/simciku'
+    mergesim(sim_path)
+    test_13.txt_convTo_json('sim_all.txt')
+
 
 
 if __name__ == "__main__":
